3caf6949dc975e88c7272c7be7d9931593fcc350
Marllaest2075 <Marllaest2075@gmail.com>
2021-03-09 21:07:57 -0700

    Authorize
    Autorisacion de usuarios

--- api/index.js
+++ api/index.js
@@ -9,6 +9,8 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology:true})
+
 app.use('/api/auth/',auth)
 
 
